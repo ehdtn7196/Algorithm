515327f3967ecd9a943944040534744b766f07d3
DSKIM <ehdtn7196@gmail.com>
2023-03-17 19:10:53 +0900

    if_for

--- A+B.py
+++ A+B.py
@@ -1,7 +1,25 @@
-input_str = input()
-A, B = input_str.split()
-A = int(A)
-B = int(B)
+A, B, C = map(int, input().split())
+if A == B and B == C:
+    print(10000 + A * 1000)
+elif A == B and B != C or A == C and A != B:
+    print(1000 + A * 100)
+elif B == C and A != B:
+    print(1000 + B * 100)
+else:
+    if A > B and A > C:
+        print(A * 100)
+    elif B > A and B > C:
+        print(B * 100)
+    elif C > A and C > B:
+        print(C * 100)
 
-if 0 < A < 10 and 0 < B < 10:
-    print(A+B)
+A, B, C = map(int, input().split())
+
+if A == B == C:
+    print(10000 + A * 1000)
+elif A == B or A == C:
+    print(1000 + A * 100)
+elif B == C:
+    print(1000 + B * 100)
+else:
+    print(max(A, B, C) * 100)
